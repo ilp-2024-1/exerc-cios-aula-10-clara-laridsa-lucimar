a7d45e7e1991b09b620241463496c7eb45e36ca8
Laridsa Sales Alves <laridsa.sales@academico.ifrn.edu.br>
2024-07-04 09:18:47 -0300

    Questão 01 Resolvida

--- exercicio_while.py
+++ exercicio_while.py
@@ -1,1 +1,8 @@
 # Questão 01 - Laridsa...
+variavel = int (input("Digite um número de 1 a 100: "))
+# numero  1 >= 100 
+while (variavel >= 1 and variavel <=100):
+    print("O número digital está dentro do intervalo entre 1 e 100.")
+    variavel = int (input("Digite um número de 1 a 100: "))
+
+print("Fim da questão 01")   
