27372d106e46e244797b5b601f44a48ed77b2bbd
Laridsa Sales Alves <laridsa.sales@escolar.ifrn.edu.br>
2024-07-17 11:13:09 -0300

    questão 04 completa

--- exercicio_while.py
+++ exercicio_while.py
@@ -1,4 +1,7 @@
 # Questão 01 - Laridsa...
+from http.client import OK
+
+
 variavel = int (input("Digite um número de 1 a 100: "))
 # numero  1 >= 100 
 while (variavel >= 1 and variavel <=100):
@@ -20,9 +23,29 @@ while True:
 print("o resultado da soma é:", soma)
 print("fim do programa")
 
+######### Questão 04 - Laridsa... #########################################################################
+print(input("Dê um palpite:"))
+palpite = range(1,100)
+total_de_tentativas = variavel
+print(palpite)
+while (palpite +100):
+      print(input("tentativa"))
+print("você digitou")
+
+if (palpite <1 or palpite >100):
+ print("voce deve escolher um número entre 1  e 100")
+
+if palpite == 35:
+    print("você acertou!")
+else:
+   
+ print("fim !")
 
 
 
+
+            
+    
 
 
 
